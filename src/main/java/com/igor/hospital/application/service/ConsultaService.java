c7b824263a7e0b62b2922cb2894ca09a0cc121de
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2025-05-23 12:06:29 -0300

    criando o salvar consulta com graphql

--- src/main/java/com/igor/hospital/application/service/ConsultaService.java
+++ src/main/java/com/igor/hospital/application/service/ConsultaService.java
@@ -7,6 +7,7 @@ import com.igor.hospital.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -21,5 +22,21 @@ public class ConsultaService {
     public List<Consulta> buscarPorPaciente(Integer pacienteId) {
         Usuario paciente = usuarioRepository.findByIdUsuario(pacienteId).get();
         return consultaRepository.findByIdPaciente(paciente);
+    }
+
+    public Consulta criarConsulta(Integer pacienteId, Integer medicoId, String dataHoraStr) {
+        Usuario paciente = usuarioRepository.findById(pacienteId)
+                .orElseThrow(() -> new RuntimeException("Paciente não encontrado"));
+
+        Usuario medico = usuarioRepository.findById(medicoId)
+                .orElseThrow(() -> new RuntimeException("Médico não encontrado"));
+
+        Consulta consulta = new Consulta();
+        consulta.setIdPaciente(paciente);
+        consulta.setIdMedico(medico);
+        consulta.setDataHora(LocalDateTime.parse(dataHoraStr));
+        consulta.setStatus("AGENDADA");
+
+        return consultaRepository.save(consulta);
     }
 }
--- src/main/java/com/igor/hospital/presentation/controller/ConsultaGraphQL.java
+++ src/main/java/com/igor/hospital/presentation/controller/ConsultaGraphQL.java
@@ -4,6 +4,7 @@ import com.igor.hospital.application.service.ConsultaService;
 import com.igor.hospital.domain.entity.Consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,5 +19,14 @@ public class ConsultaGraphQL {
     @QueryMapping
     public List<Consulta> consultasPorPaciente(@Argument Integer pacienteId) {
         return consultaService.buscarPorPaciente(pacienteId);
+    }
+
+    @MutationMapping
+    public Consulta criarConsulta(
+            @Argument Integer pacienteId,
+            @Argument Integer medicoId,
+            @Argument String dataHora
+    ) {
+        return consultaService.criarConsulta(pacienteId, medicoId, dataHora);
     }
 }
